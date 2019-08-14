b0bcf1054283fd56ea147dabfa61aad409579f0f
Jakob Brobak Petersen <jakobbrobak@Jakobs-MBP.mydomain>
2019-08-14 15:55:10 +0200

    Tilføjelse af component og resolver

--- DatingApp-SPA/src/app/routes.ts
+++ DatingApp-SPA/src/app/routes.ts
@@ -1,3 +1,4 @@
+import { MemberEditComponent } from './members/member-edit/member-edit.component';
 import { MemberDetailResolver } from './_resolvers/member-detail.resolver';
 import { AuthGuard } from './_guards/auth.guard';
 import { MessagesComponent } from './messages/messages.component';
@@ -7,6 +8,7 @@ import { Routes } from '@angular/router';
 import { ListsComponent } from './lists/lists.component';
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 import { MemberListResolver } from './_resolvers/member-list.resolver';
+import { MemberEditResolver } from './_resolvers/member-edit.resolver';
 
 export const appRoutes: Routes = [
   { path: '', component: HomeComponent },
@@ -15,12 +17,17 @@ export const appRoutes: Routes = [
     runGuardsAndResolvers: 'always',
     canActivate: [AuthGuard],
     children: [
-      { path: 'members', component: MemberListComponent, resolve: {users: MemberListResolver}  },
+      {
+        path: 'members',
+        component: MemberListComponent,
+        resolve: { users: MemberListResolver }
+      },
       {
         path: 'members/:id',
         component: MemberDetailComponent,
         resolve: { user: MemberDetailResolver }
       },
+      { path: 'member/edit', component: MemberEditComponent, resolve: {user: MemberEditResolver} },
       { path: 'messages', component: MessagesComponent },
       { path: 'lists', component: ListsComponent }
     ]
